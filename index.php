bec800d46e328e8fbd5c5c5cbd606eab9d18b889
Yoeri Van Wassenhove <yoerivw@hotmail.com>
2018-09-10 12:23:37 +0200

    Commit 10/09/2018

--- index.php
+++ index.php
@@ -1,64 +1,81 @@
-<?php 
-$page_title = 'Welcome to BRP';
-include ('assets/html/header.html');
-?>
+<?php $page_title = 'Welcome to BRP'; include('assets/html/header.html');?>
 <!-- ============  -->
 <!--  HERO SECTION -->
 <!-- ============  -->
-<section id="hero" data-type="background" data-speed="5">
-    <div class="container h-100">
-        <div class="row h-100 justify-content-center align-items-center">
+<section id="hero">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
             <div class="col-12">
-            <h1 id="hero-header" >Bergman Ross and partners <br><span id="hero-header_big">radiologists</span></h1>
+                <h1 id="hero-header">Bergman Ross and partners <br><span id="hero-header_big">radiologists</span></h1>
+            </div>
+            <!-- End Div -->
+        </div>
+        <!-- end row -->
 
-            <div class="row">
+
+        <div class="row justify-content-center align-items-center hero-row_buttons">
             <div class="col-6">
-                <button class="btn btn-primary btn-lg">Patient Portal</button>
-            </div> <!-- end col -->
+                <button class="btn btn-primary btn-lg btn-patient">Patient Portal</button>
+            </div>
+            <!-- end col -->
             <div class="col-6">
-            <button class="btn btn-primary btn-lg">Physician Portal</button>
-            </div><!-- end col -->
-        </div><!-- end row -->
+                <button class="btn btn-primary btn-lg btn-physician">Physician Portal</button>
+            </div>
 
-            </div> <!-- End Div -->
-        </div><!-- end row -->
 
-        
 
-    </div><!-- end container -->
+        </div>
+        <!-- end row -->
 
-</section> <!-- End HERO SECTION -->
+
+
+
+
+    </div>
+    <!-- end container -->
+
+</section>
+<!-- End HERO SECTION -->
 
 <!-- ============  -->
 <!--  INTERNAL LINKING SECTION -->
 <!-- ============  -->
 <section id="link-cards">
 
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-            <div class="card card-image">
-                <img class="card-img" src="assets/img/our-services.jpg" alt="Card Image">
-                <div class="card-img-overlay">
-                    <button>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="card card-image">
+                    <img class="card-img" src="assets/img/our-services.jpg" alt="Card Image">
 
-                    </button>
-                </div><!-- end card body -->
-            </div><!-- end card -->
-        </div><!-- end col -->
+                    <div class="card-img-overlay">
+                    </div>
+                    <!-- end card body -->
+                    <a href="#" class="btn btn-danger btn-lg link-button">Patient Portal</a>
+                </div>
+                <!-- end card -->
+            </div>
+            <!-- end col -->
 
-        <div class="col-6">
-            <div class="card card-image">
-                <img class="card-img" src="assets/img/our-practioners.jpg" alt="Card Image">
-                <div class="card-img-overlay">
-                    
-                </div><!-- end card body -->
-            </div><!-- end card -->    
+            <div class="col-6">
+                <div class="card card-image">
+                    <img class="card-img" src="assets/img/our-practioners.jpg" alt="Card Image">
 
-        </div><!-- end col -->
-    </div><!-- end row -->
-</div><!-- end container -->
-</section> <!-- End INTERNAL LINKING SECTION -->
+                    <div class="card-img-overlay">
+                    </div>
+                    <!-- end card body -->
+                    <a href="#" class="btn btn-danger btn-lg link-button">Physicial Portal</a>
+                </div>
+                <!-- end card -->
+
+            </div>
+            <!-- end col -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container -->
+</section>
+<!-- End INTERNAL LINKING SECTION -->
 
 <!-- ============  -->
 <!--  WHO ARE WE -->
@@ -72,84 +89,164 @@ include ('assets/html/header.html');
                 <p>Bergman, Ross &amp; Partners Radiology is a private radiology practice with branches in the Western Cape and Gauteng. We offer a wide variety of the most technologically advanced imaging.</p><br>
                 <p>We have a client-centered approach, which means that we guide the client through every step of the way with the utmost care and sensitivity. Our priority is the client's safety and comfort.</p><br>
                 <p>We encourage constructive engagement with referring physicians. We believe this to be the best way to create an inclusive and accommodating environment.</p>
-            </div><!-- end col -->
+            </div>
+            <!-- end col -->
             <div class="col-6">
                 <img src="assets/img/brp-building.jpg" alt="The bergman ross and partners building">
             </div>
-        </div> <!-- end row -->
-    </div><!-- end container -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container -->
 
-</section> <!-- End WHO ARE WE SECTION -->
+</section>
+<!-- End WHO ARE WE SECTION -->
 
 <!-- ============  -->
 <!--  NEWS AND UPCOMING EVENTS SECTION -->
 <!-- ============  -->
-<section id="news-events_home">
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
+<section id="news-events">
+    <div class="container news-events_container">
+        <div class="row news-events_row">
+            <!-- NEWS COLUMN -->
+            <div class="col-7">
+                <h2>News</h2>
+                <div class="row news-events_row-news">
+                    <div class="col-6">
+                        <div class="card card_news-events">
+                            <img class="card-img-top" src="assets/img/news-1.jpg" alt="Card image cap">
+                            <div class="card-body card-body_news">
+                                <a href="#" class="card-link">Breast Cancer awareness</a>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+                    </div>
+                    <!-- end col -->
+
+                    <div class="col-6">
+                        <div class="card card_news-events">
+                            <img class="card-img-top" src="assets/img/news-2.jpg" alt="Card image cap">
+                            <div class="card-body card-body_news">
+                                <a href="#" class="card-link">Radiation: What You need to know</a>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+
+                    </div>
+                    <!-- end col -->
+
+                </div>
+                <!--  end row -->
+
+                <div class="row news-events_row-news">
+                    <div class="col-6">
+                        <div class="card card_news-events">
+                            <img class="card-img-top" src="assets/img/news-3.jpg" alt="Card image cap">
+                            <div class="card-body card-body_news">
+                                <a href="#" class="card-link">The science of a healthy pregnancy</a>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+
+                    </div>
+                    <!-- end col -->
+
+                    <div class="col-6">
+                        <div class="card card_news-events">
+                            <img class="card-img-top" src="assets/img/news-4.jpg" alt="Card image cap">
+                            <div class="card-body card-body_news">
+                                <a href="#" class="card-link">Advancements in radiology</a>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+
+                    </div>
+                    <!-- end col -->
+
+                </div>
+                <!-- end row -->
+
+                <button class="btn btn-lg btn-danger btn-block">More Topics</button>
+
+            </div>
+            <!-- end col-7 -->
+
+
+            <!-- UPCOMING EVENTS COLUMN -->
+            <div class="col-5">
+                <h2 class="text-center">Upcoming events</h2>
+                <div class="row news-events_row-events">
+                    <div class="col-6 offset-2">
+                        <div class="card card_news-events">
+                            <div class="card-header">Radiology Conference</div>
+                            <div class="card-body">
+                                <a href="" class="link-text">Featuring comprehensive talks on the latest developments in radiology</a>
+                                <p class="card-text">25 August 2018</p>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+
+                    </div>
+                    <!-- end col -->
+
+                </div>
+                <!-- end row -->
+
                 <div class="row">
-                    <div class="col-6">
-                        <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                        <a href="#" class="btn btn-primary">Breast Cancer awareness</a>
-                        </div> <!-- end card body -->
-                        </div><!-- end card -->
-                    </div> <!-- end col -->
+                    <div class="col-6 offset-2">
+                        <div class="card card_news-events">
+                            <div class="card-header">BRP Presentation</div>
+                            <div class="card-body">
+                                <a href="" class="link-text">One-hour talk on the importance of technological advancement in radiology</a>
+                                <p class="card-text">3 September 2018</p>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
 
-                    <div class="col-6">
-                    <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                        <a href="#" class="btn btn-primary">Breast Cancer awareness</a>
-                        </div> <!-- end card body -->
-                        </div><!-- end card -->
+                    </div>
+                    <!-- end col -->
 
-                    </div><!-- end col -->
-
-                </div> <!--  end row -->
+                </div>
+                <!-- end row -->
 
                 <div class="row">
-                    <div class="col-6">
-                    <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                        <a href="#" class="btn btn-primary">Breast Cancer awareness</a>
-                        </div> <!-- end card body -->
-                        </div><!-- end card -->
+                    <div class="col-6 offset-2">
+                        <div class="card card_news-events">
+                            <div class="card-header">Cancer Research Fundraiser</div>
+                            <div class="card-body">
+                                <a href="" class="link-text">5km run to raise funds for those whose lives depend on it</a>
+                                <p class="card-text">10 October 2018</p>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
 
-                    </div><!-- end col -->
+                    </div>
+                    <!-- end col -->
 
-                    <div class="col-6">
-                    <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="..." alt="Card image cap">
-                        <div class="card-body">
-                        <a href="#" class="btn btn-primary">Breast Cancer awareness</a>
-                        </div> <!-- end card body -->
-                        </div><!-- end card -->
-
-                    </div><!-- end col -->
-
-                </div><!-- end row -->
-
-
-              
-            </div> <!-- end col-8 -->
-
-            <div class="col-4">
-                
-            </div> <!-- end col -->
-
-        </div><!-- end row -->
-
-    </div> <!-- end container -->
-
-
-</section> <!-- End NEWS AND UPCOMING EVENTS SECTION -->
-
-
-<?php include ('assets/html/footer.html'); ?>
+                </div>
+                <!-- end row -->
 
 
 
+            </div>
+            <!-- end col -->
+
+        </div>
+        <!-- end row -->
+
+    </div>
+    <!-- end container -->
+
+
+</section>
+<!-- End NEWS AND UPCOMING EVENTS SECTION -->
+
+
+<?php include('assets/html/footer.html') ?>
